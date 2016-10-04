8f9526add5b3fb1a3bb855a31b4ce2d59b132330
Valery Bilyi <bilyi.valery@gmail.com>
2016-10-04 21:39:53 +0300

    Add files via upload

---
+++ Blog.View/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Blog.View
+{
+    public class BundleConfig
+    {
+       
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/scripts/jquery.unobtrusive-ajax.js",
+                        "~/scripts/jquery-ui-1.12.1.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                       "~/Scripts/javascript.js*"));
+
+            
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/Site.css",
+                      "~/Content/themes/base/all.css"));
+        }
+    }
+}
